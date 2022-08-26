273a45ec180ea08dda0ebc6bd325fefe62a40264
Seif Wael <seifeldin7666@gmail.com>
2022-08-26 15:10:57 +0200

    Python UDP server and client added

---
+++ SessionExamples/UDP/python/client.py
@@ -0,0 +1,17 @@
+import socket as sk
+import os
+
+
+SERVER_IP = input('Enter server IP address: ')
+SERVER_PORT = int(input('Enter Server port: '))
+sock= sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+
+while True:
+    number= int(input('Enter Frequency: '))
+    data= [0,0]
+    data[0]= number// 256
+    data[1]= number % 256
+    address= (SERVER_IP,SERVER_PORT)
+    sock.sendto(bytearray(data), address)
+
+    
---
+++ SessionExamples/UDP/python/server.py
@@ -0,0 +1,22 @@
+import socket as sk
+import os
+
+
+SERVER_IP = input('Enter server IP address: ')
+SERVER_PORT = int(input('Enter Server port: '))
+
+sock = sk.socket(sk.AF_INET,  sk.SOCK_DGRAM)
+sock.bind(('', SERVER_PORT))
+
+duration = 1  # seconds
+
+print('Server listening...')
+while True:
+    data, addr = sock.recvfrom(1024) 
+    print(data)
+    number= min(800,data[0]*256+data[1])
+    print(number)
+    os.system('play -nq -t alsa synth {} sine {}'.format(duration, number))
+
+
+    
